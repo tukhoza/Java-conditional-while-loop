4bc79e5f3c1e5d7dbce9cdaabd2af90130cf3392
tukhoza <39373673+tukhoza@users.noreply.github.com>
2023-06-03 11:33:58 +0100

    Conditional while loop

---
+++ Java while loop condition/src/app/App.java
@@ -0,0 +1,27 @@
+package app;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int count1 = 0;
+		
+		while (count1 < 10){
+			
+			System.out.println("The increment number :" + count1);
+			count1 = count1 + 1;
+		}
+		
+		
+		System.out.println("\nPrint numbers from 1 to 10 \n");
+		int count2 = 1;
+		
+		while (count2 <= 10){
+			
+			System.out.println("Number : " + count2);
+			count2 += 1;
+			
+		}
+	}
+
+}
